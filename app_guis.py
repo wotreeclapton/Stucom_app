344f7f931fa1900dd7822e18cd67ce66057a7d99
wotreeclapton <wotreeclapton@hotmail.com>
2020-02-20 09:16:10 +0700

    Set up files

--- app_guis.py
+++ app_guis.py
@@ -1,1 +1,10 @@
+#! python 3
+'''
+STUDENT COMMENTS APPLICATION GUIS developed by Mr Steven J walden
+(Stucom)
+    Feb. 2020
+    SAMROIYOD, PRACHUAP KIRI KHAN, THAILAND
+[See License.txt file]
+'''
 
+import sys
--- stucom_main.py
+++ stucom_main.py
@@ -1,1 +1,28 @@
+#! python 3
+'''
+STUDENT COMMENTS APPLICATION LAUNCHER developed by Mr Steven J walden
+(Stucom)
+    Feb. 2020
+    SAMROIYOD, PRACHUAP KIRI KHAN, THAILAND
+[See License.txt file]
+'''
 
+__author__ = 'Mr Steven J Walden'
+__version__ = '1.0.0'
+
+import sys
+import os
+
+
+
+print(sys.executable)
+
+if __name__ == '__main__':
+    # print("Qt version:", QT_VERSION_STR)
+    print("Author:", __author__)
+    print("App version:",__version__)
+
+ #    app = QtWidgets.QApplication(sys.argv)
+ #    main_app = App()
+
+	# sys.exit(app.exec_())
